149d161b679020d1f7bac654fb708ab98301ae34
LorenzoLim <renzo-lim@hotmail.com>
2017-12-26 12:39:53 +1000

    .env added and connected to mLab

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/users', {useMongoClient:true});
+const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds163836.mlab.com:63836/easy-log-system`, {useMongoClient:true});
 mongoose.Promise = Promise
 
 module.exports = { mongoose, db }
