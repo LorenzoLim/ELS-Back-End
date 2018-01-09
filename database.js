e6454a94817511195e9b8c7148df2d513e122f63
LorenzoLim <renzo-lim@hotmail.com>
2018-01-09 22:05:24 +1000

    Console log for dotenv test removed

--- database.js
+++ database.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 // Use the promise functionality built into Node
 mongoose.Promise = global.Promise
 
-console.log(`${process.env.DB_USER}`)
-
 const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds163836.mlab.com:63836/easy-log-system`, {useMongoClient:true})
 .then(() => {
   console.log('Successfully connected to database!')
