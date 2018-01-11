82c79e7dedcca7daf3eac1403db0745406d8fd60
LorenzoLim <renzo-lim@hotmail.com>
2018-01-11 10:38:25 +1000

    Moves the User routes to the user.js

--- routes/index.js
+++ routes/index.js
@@ -7,47 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Find user by ID */
-router.get('/users/:id', (req, res) =>{
-  User.findById(req.params.id).then((user) =>{
-    console.log(user);
-    res.send(user);
-  })
-});
-
-/* Returns all users */
-router.get('/users', (req, res) => {
-  User.find().then((users) =>{
-    res.json(users);
-  });
-});
-
-/* Creates new user*/
-router.post('/users', (req, res) => {
-  User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-    role: req.body.role
-  }).then(() => {
-    res.send('Success')
-  })
-});
-
-/* Update data from database */
-router.put('/users/:id', function (req,res) {
-  User.findOneAndUpdate(req.params.id, req.body).then((user) =>{
-    res.send('Updated');
-  });
-});
-
-/* Delete data from database */
-router.delete('/users/:id', function (req,res) {
-  User.findByIdAndRemove(req.params.id).then((user) =>{
-    res.send('Deleted');
-  });
-});
-
-
-  module.exports = router;
+module.exports = router;
